e31d84d52c64399b669e6907487ea72b2618917c
Joey <joeytan233@gmail.com>
2023-03-06 14:24:12 -0500

    Updated schema

--- Backend/gotta-go-schema.sql
+++ Backend/gotta-go-schema.sql
@@ -1,10 +1,41 @@
+/*
+Name
+Gender
+Account
+Password
+Email
+Profile picture
+Collection
+
+Bathroom information:
+Name of the bathroom
+Address
+Grading
+Direction/description
+
+Monsters:
+Name
+Quality
+Size
+Health?
+Attack?
+*/
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    gender TEXT,
+    password TEXT,
+    email TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE bathrooms(
     id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    grade TEXT,
+    description TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
