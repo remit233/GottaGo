03557f6e1d3a112e42c3646cf4db4f9152e00302
Steven Salto <ssalto2702@gmail.com>
2023-03-09 00:39:55 -0500

    Fixes to navigation. Added indents

--- website/src/components/App/App.js
+++ website/src/components/App/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-//import Home from './components/pages/Home'
-
-
 
 function App() {
   return (
-    <>
-    <Router>
-    <Navbar/>
-    <Routes>
-      <Route path='/' />
-    </Routes>
-    </Router>
-      
-    </>
+    <div className='app'>
+      <Navbar/>
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      <Footer/>
+    </div>
   );
 }
 
--- website/src/index.js
+++ website/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
