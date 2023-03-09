6486a3910e5772bea38ea2ec587c91dad197e2ad
Steven Salto <ssalto2702@gmail.com>
2023-03-08 21:50:13 -0500

    Added react-router-dom module for navigation

--- website/src/index.js
+++ website/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Navbar from './components/Navbar/Navbar';
+import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
