904df760f69b524381434c2662d1b2d5ed31fb53
Steven Salto <ssalto2702@gmail.com>
2023-03-09 20:49:03 -0500

    Edited css variables

--- website/src/index.js
+++ website/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
