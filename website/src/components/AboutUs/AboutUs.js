820eead1aeb469e9957a18af77b8d7171edfd96f
Steven Salto <ssalto2702@gmail.com>
2023-03-23 01:19:44 -0400

    Added forms for user feedback in footer.

--- website/src/components/AboutUs/AboutUs.js
+++ website/src/components/AboutUs/AboutUs.js
@@ -3,11 +3,8 @@ import './AboutUs.css'
 export default function AboutUs() {
     return(
         <div className='about-us section'>
-            <div className='heading'>Our Goals</div>
-            <div className='body'></div>
-            <div className='subheading'>Improving Accessibility</div>
-            <div className='subheading'>Improving Accessibility</div>
-
+            <h1 className='about-us-header1'>why we do what we do</h1>
+            <p  className='explanation'>&emsp;In today's world, we rely on technology for almost everything, from ordering food to finding our way around a new city. However, there is one aspect of our daily lives that has been largely overlooked: our bathroom habits. A bathroom app would be a useful tool to help us manage our daily bathroom needs. The app could help us find the nearest public restroom, provide information on the cleanliness of different facilities, and even offer suggestions for eco-friendly bathroom products. Additionally, a bathroom app could be a useful tool for people with medical conditions that require frequent bathroom trips, such as bladder or bowel disorders. With a bathroom app, we could feel more confident and comfortable when we're out and about, knowing that we have access to the information we need to take care of our bathroom needs.</p>
         </div>
     )
 }
