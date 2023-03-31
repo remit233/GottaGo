ae507145c078a8c566a6b587ffb8dc6907b873fd
Steven Salto <ssalto2702@gmail.com>
2023-03-31 01:23:59 -0400

    Added marker to map

--- website/src/components/Home/Home.js
+++ website/src/components/Home/Home.js
@@ -1,6 +1,13 @@
 import './Home.css'
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
 export default function Home() {
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: ""
+    });
+
+    if(!isLoaded) return <div>loading...</div>;
+
     return (
         <div className="home section">
             <h1 className='home-header1'>let's go weewee :3</h1>
@@ -10,7 +17,10 @@ export default function Home() {
                 <input type="text" name="search" placeholder='where da bathroom at...'/>
             </form>
         
-            <iframe src="https://www.google.com/maps/embed/v1/view?key=&center=40.76930565282415,-73.96456718034894&zoom=13"></iframe>
+           <GoogleMap zoom={15} center={{lat:40.768497649503715, lng:-73.96421422429842}} mapContainerClassName="map-container">
+                <Marker position={{lat:40.768497649503715, lng:-73.96421422429842}}/>
+                <Marker position={{lat:41.768497649503715, lng:-73.96421422429842}}/>
+           </GoogleMap>
         </div>
     )
 }
