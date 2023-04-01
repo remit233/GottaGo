02ad7128cc84c19fe474e3c496eb32b205842d1a
Steven Salto <ssalto2702@gmail.com>
2023-03-31 22:06:18 -0400

    Add centering map on user location

--- website/src/components/Home/Home.js
+++ website/src/components/Home/Home.js
@@ -1,13 +1,22 @@
 import './Home.css'
 import Markers from '../Markers/Markers'
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api"
+import { GoogleMap, useLoadScript } from "@react-google-maps/api"
+import { useState } from 'react';
 
 export default function Home() {
+    const [userLatitude, setUserLatitude] = useState(40.768497649503715);
+    const [userLongitude, setUserLongitude] = useState(-73.96421422429842);
+
     const {isLoaded} = useLoadScript({
         googleMapsApiKey: ""
     });
 
     if(!isLoaded) return <div>loading...</div>;
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+        setUserLatitude(pos.coords.latitude);
+        setUserLongitude(pos.coords.longitude);
+    })
 
     return (
         <div className="home section">
@@ -18,8 +27,8 @@ export default function Home() {
                 <input type="text" name="search" placeholder='where da bathroom at...'/>
             </form>
         
-           <GoogleMap zoom={15} center={{lat:40.768497649503715, lng:-73.96421422429842}} mapContainerClassName="map-container">
-                <Markers/>
+           <GoogleMap zoom={15} center={{lat:userLatitude, lng:userLongitude}} mapContainerClassName="map-container">
+              <Markers/>
            </GoogleMap>
         </div>
     )
--- website/src/components/Markers/Markers.js
+++ website/src/components/Markers/Markers.js
@@ -5,14 +5,22 @@ import { useState, useEffect } from "react"
 export default function Markers() {
     const [nearbyBathrooms, setNearbyBathrooms] = useState([])
 
+    const [userLatitude, setUserLatitude] = useState(40.768497649503715);
+    const [userLongitude, setUserLongitude] = useState(-73.96421422429842);
+
+    
     useEffect(() => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            setUserLatitude(pos.coords.latitude);
+            setUserLongitude(pos.coords.longitude);
+        })
         function fetchBathrooms() {
             axios({
                 method:'POST',
                 url:'http://localhost:3001/bathroom/get-nearby', 
                 
-                data: {	lat:40.768497649503715, 
-                        lng:-73.96421422429842}, 
+                data: {	lat: userLatitude, 
+                        lng: userLongitude}, 
                 headers:{'Content-Type': 'application/json'} })
             .then((res) => {
                 setNearbyBathrooms(res.data)
@@ -20,8 +28,11 @@ export default function Markers() {
         }
         fetchBathrooms();
     },[])
+
     return (
-        <>{
+        <>
+        <MarkerF position={{lat:userLatitude, lng:userLongitude}} label={{ text: 'U R Here :3' }}/>
+        {
             nearbyBathrooms.map(bathroom => {
                 return(<MarkerF position={{lat:bathroom.lat, lng:bathroom.lng}}/>)
             })
