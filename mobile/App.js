569498dacf5ba1f3d3bd098af7a7221b00941141
Steven Salto <ssalto2702@gmail.com>
2023-04-01 16:55:46 -0400

    Implement nearby markers

--- mobile/App.js
+++ mobile/App.js
@@ -1,28 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import MapView, { Callout, Marker } from "react-native-maps";
+import {useState, useEffect} from 'react';
+import * as Location from 'expo-location';
+import axios from 'axios'
 
 export default function App() {
+  const [nearbyBathrooms, setNearbyBathrooms] = useState([])
+
+  const [userLatitude, setUserLatitude] = useState(40.767807649503715);
+  const [userLongitude, setUserLongitude] = useState(-73.96451422429842);
+
   
+  useEffect(() => {
+    async function requestGeoPermissions() {
+      await Location.requestForegroundPermissionsAsync()
+      setUserLatitude((await Location.getCurrentPositionAsync()).coords.latitude)
+      setUserLongitude((await Location.getCurrentPositionAsync()).coords.longitude)
+      fetchBathrooms();
+      console.log(userLatitude, userLongitude)
+    }
+    function fetchBathrooms() {
+      axios({
+          method:'POST',
+          url:'http://10.0.2.2:3001/bathroom/get-nearby', 
+          data: {	lat: userLatitude, 
+                  lng: userLongitude}, 
+          headers:{'Content-Type': 'application/json'} })
+      .then((res) => {
+          setNearbyBathrooms(res.data)
+      })
+    }
+    requestGeoPermissions();
+  }, [])
 
   return (
     <View style={styles.container}>
       <MapView 
         style={styles.map} 
         initialRegion={{
-          latitude:40.76799008313369,
-          longitude:-73.96456128222825,
+          latitude:userLatitude,
+          longitude:userLongitude,
           latitudeDelta: 0.09,
           longitudeDelta: 0.04
         }}>
-          <Marker coordinate={{latitude:37,
-          longitude:-122,}}>
+        <Marker coordinate={{latitude:userLatitude, longitude:userLongitude}}>
+          <Callout>
+            <Text>U R Here :3</Text>
+          </Callout>
+        </Marker>
+        {nearbyBathrooms.map((bathroom)=> {
+          console.log("latititit", bathroom.lat);
+          return(<Marker coordinate={{latitude:bathroom.lat, longitude:bathroom.lng}}>
             <Callout>
-              <Text>Hewwo :3</Text>
+              <Text>Hewwowoo</Text>
             </Callout>
-          </Marker>
-        </MapView>
-      <Text>Hewwo :3</Text>
+          </Marker>)
+        })}
+      </MapView>
+      <Text>Hewwoooo :3</Text>
       <Text>Welcome to Bathroom Tracker</Text>
       <StatusBar style="auto" />
     </View>
