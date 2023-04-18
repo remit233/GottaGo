8866042c3f0732238e2be79962e0a1217e8a0381
Joey <joeytan233@gmail.com>
2023-04-17 22:29:45 -0400

    Border style on profile

--- mobile/components/screens/ProfileScreen.js
+++ mobile/components/screens/ProfileScreen.js
@@ -14,7 +14,50 @@ export default function GameScreen({navigation}){
             <Text onPress = { () => alert('Profile!!')}
             style={{frontSize: 26,  fontWeight: 'bold', padding:10}}>STEVEN HERE</Text>
         </View>
-        
+
+        <View  style={{
+            alignSelf: 'center',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            backgroundColor: '#FFF',
+            width: '90%',
+            padding: 20,
+            paddingBottom: 10,
+            shadowOpacity: 80,
+            shadowColor: '#743EA7',
+            borderRadius: 10,
+            elevation: 15,
+            marginTop: 20,
+            marginBottom: 40
+        }}>
+            <Text
+            style={{frontSize: 100,fontWeight:'bold', padding:20}}> Recent Viewed Bathroom </Text>
+        </View>
+
+        <View style={{
+            alignSelf: 'center',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            backgroundColor: '#FFF',
+            width: '90%',
+            padding: 20,
+            paddingBottom: 10,
+            shadowOpacity: 80,
+            shadowColor: '#743EA7',
+            borderRadius: 10,
+            elevation: 15,
+            marginTop: 20,
+            marginBottom: 40
+        }}>
+            <Text
+            style={{frontSize: 26, fontWeight:'bold', padding:20}}>Setting</Text>
+        </View>
+
+        <View style={{alignItems: 'center'}}>
+            <Text
+            style={{frontSize: 100, padding:20}}>Registered date: 04/04/2023</Text>
+        </View>
+
     </ScrollView>
     );
 }
