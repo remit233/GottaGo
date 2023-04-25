dd48d475a2874e117b3900c1c8f0b3d9cfcdc8f9
wchen2296 <53542192+wchen2296@users.noreply.github.com>
2023-04-25 15:41:26 -0400

    chat changes

--- mobile/components/screens/ChatScreen.js
+++ mobile/components/screens/ChatScreen.js
@@ -1,46 +1,100 @@
-//Detail screen
-import * as React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-
-import MessageModal from './ChatScreens/MessageModal'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, Keyboard, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import MessageModal from './ChatScreens/MessageModal';
 import PostBox from './ChatScreens/PostBox';
-export default function ChatScreen({navigation}){
-    return(
-        <View style={styles.container}>
-            <View style={styles.wrapper}>
-                <View style={styles.header}>
-                    <Text style={styles.text}>CurrentBathroom</Text>
-                </View>
-                <PostBox title={'Is the bathroom clean'} author={'Joey :P'}/>
-                <PostBox title={'Make font size smaller as text exceeds max heigght'} author={'Lewwis :3'}/>
-            </View>
-            <MessageModal/>
+
+export default function ChatScreen({ navigation }) {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = () => {
+    if (message.trim()) {
+      setMessages([...messages, { text: message, author: 'Me' }]);
+      setMessage('');
+      Keyboard.dismiss();
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : null} style={styles.container} keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}>
+
+      <View style={styles.wrapper}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionicons name="chevron-back-outline" size={24} color="black" />
+          </TouchableOpacity>
+          <Text style={styles.text}>Current Bathroom</Text>
         </View>
-    );
+        <View style={styles.chatContainer}>
+          {messages.map((msg, index) => (
+            <PostBox key={index} title={msg.text} author={msg.author} />
+          ))}
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Type a message"
+            value={message}
+            onChangeText={(text) => setMessage(text)}
+            onSubmitEditing={sendMessage}
+            returnKeyType="send"
+          />
+          <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+            <Ionicons name="send-outline" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        flex:1,
-        justifyContent:'center',
-        alignItems: 'center',
-        backgroundColor:'#f2f2f2'
-    },
-    wrapper: {
-        flex:1,
-        height:'96%',
-        width:'98%',
-        justifyContent:'flex-start',
-        alignItems:'center'
-    },
-    header: {
-        height:50,
-        width:'100%',
-        alignItems:'flex-start',
-        justifyContent:'center'
-    },
-    text: {
-        fontSize:16,
-        textDecorationLine:'underline'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  header: {
+    height: 50,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#d3d3d3',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  chatContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#d3d3d3',
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginRight: 10,
+  },
+  sendButton: {
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    padding: 10,
+  },
+});
--- mobile/components/screens/ProfileScreen.js
+++ mobile/components/screens/ProfileScreen.js
@@ -1,63 +1,116 @@
-import * as React from 'react';
-import { View, Text, ScrollView ,Image} from 'react-native';
+import React from 'react';
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
 
-export default function GameScreen({navigation}){
-    return(
-    <ScrollView>
-        <View style={{ padding:10, width:'100%', backgroundColor: '#743EA7', height: 150, alignItems: 'center', justifyContent: 'center',}}>
+const GameScreen = ({ navigation }) => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerBackground} />
+      </View>
 
-        </View>
+      <View style={styles.profileContainer}>
+        <Image
+          source={require('./Images/profilepic.png')}
+          style={styles.profileImage}
+        />
+        <Text style={styles.profileName} onPress={() => alert('Profile!!')}>
+          STEVEN HERE
+        </Text>
+      </View>
 
-        <View style={{alignItems: 'center'}}>
-            <Image source={require('./Images/profilepic.png')} style={{width:140, height:140,
-        borderRadius:100, marginTop: -70}}/>
-            <Text onPress = { () => alert('Profile!!')}
-            style={{frontSize: 26,  fontWeight: 'bold', padding:10}}>STEVEN HERE</Text>
-        </View>
+      <View style={styles.recentlyViewedContainer}>
+        <Text style={styles.recentlyViewedTitle}>Recently Viewed Bathroom</Text>
+      </View>
 
-        <View  style={{
-            alignSelf: 'center',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            backgroundColor: '#FFF',
-            width: '90%',
-            padding: 20,
-            paddingBottom: 10,
-            shadowOpacity: 80,
-            shadowColor: '#743EA7',
-            borderRadius: 10,
-            elevation: 15,
-            marginTop: 20,
-            marginBottom: 40
-        }}>
-            <Text
-            style={{frontSize: 100,fontWeight:'bold', padding:20}}> Recent Viewed Bathroom </Text>
-        </View>
+      <View style={styles.settingContainer}>
+        <Text style={styles.settingTitle}>Settings</Text>
+      </View>
 
-        <View style={{
-            alignSelf: 'center',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            backgroundColor: '#FFF',
-            width: '90%',
-            padding: 20,
-            paddingBottom: 10,
-            shadowOpacity: 80,
-            shadowColor: '#743EA7',
-            borderRadius: 10,
-            elevation: 15,
-            marginTop: 20,
-            marginBottom: 40
-        }}>
-            <Text
-            style={{frontSize: 26, fontWeight:'bold', padding:20}}>Setting</Text>
-        </View>
-
-        <View style={{alignItems: 'center'}}>
-            <Text
-            style={{frontSize: 100, padding:20}}>Registered date: 04/04/2023</Text>
-        </View>
-
+      <View style={styles.registeredContainer}>
+        <Text style={styles.registeredTitle}>Registered Date: 04/04/2023</Text>
+      </View>
     </ScrollView>
-    );
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerBackground: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#743EA7',
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginTop: -70,
+  },
+  profileImage: {
+    width: 140,
+    height: 140,
+    borderRadius: 100,
+  },
+  profileName: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  recentlyViewedContainer: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    width: '90%',
+    padding: 20,
+    paddingBottom: 10,
+    shadowOpacity: 0.8,
+    shadowColor: '#743EA7',
+    borderRadius: 10,
+    elevation: 15,
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  recentlyViewedTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+    textAlign: 'center',
+  },
+  settingContainer: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    width: '90%',
+    padding: 20,
+    paddingBottom: 10,
+    shadowOpacity: 0.8,
+    shadowColor: '#743EA7',
+    borderRadius: 10,
+    elevation: 15,
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  settingTitle: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  registeredContainer: {
+    alignItems: 'center',
+  },
+  registeredTitle: {
+    fontSize: 20,
+    padding: 20,
+  },
+});
+
+export default GameScreen;
