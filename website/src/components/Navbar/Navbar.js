ea73bcae52b5db65d5ecb93058e66649c3fd73f4
wchen2296 <53542192+wchen2296@users.noreply.github.com>
2023-05-08 07:51:28 -0400

    add fix markers

--- website/src/components/Navbar/Navbar.js
+++ website/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import useAuth from '../auth/useAuth'
+import useAuth from '../auth/useAuth.js'
 
 function Navbar() {
   const [renderNavLinks, setRenderNavLinks] = useState(true);
